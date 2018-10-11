cf7d07dbea721abcc20f4b6fc506c89d2c1ed5c1
independnt <gonzalezdennis6@gmail.com>
2018-10-11 18:07:00 -0400

    created custom styled divs

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components'
 
-const CustomElement = styled.div`
-  color: green;
+const Logo = styled.div`
+  font-size: 1.5em;
+`
+const ControlButton = styled.div `
+
+`
+
+const AppLayout = styled.div `
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
 `
 
 class App extends Component {
   render() {
     return (
-      <CustomElement>
-        CryptoDash
-      </CustomElement>
+      <AppLayout>
+        <Logo>
+          CryptoDash
+        </Logo>
+        <ControlButton>
+          Dashboard
+        </ControlButton>
+        <ControlButton>
+        Settings
+        </ControlButton>
+      </AppLayout>
     );
   }
 }
