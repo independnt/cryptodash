66fe88373795c76f7efe078061bf9c53a6324c57
independnt <gonzalezdennis6@gmail.com>
2018-10-11 17:47:12 -0400

    removed logo, added variable for styled components, super cool

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components'
 
+const CustomElement = styled.div`
+  color: green;
+`
+
 class App extends Component {
   render() {
     return (
-      <div>
+      <CustomElement>
         CryptoDash
-      </div>
+      </CustomElement>
     );
   }
 }
