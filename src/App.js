cc789989f94a3d08d7988f25b77c9b5a99877096
independnt <gonzalezdennis6@gmail.com>
2018-10-27 10:10:39 -0400

    refactor of navbar, moved to seperate file called appbar.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import styled, {css} from 'styled-components'
-
-const Logo = styled.div`
-  font-size: 1.5em;
-`
-const ControlButton = styled.div `
-  cursor: pointer;
-  ${props => props.active && css`
-    text-shadow: 0px 0px 60px #03ff03
-  `}
-`
+import AppBar from './AppBar.js'
 
 const AppLayout = styled.div `
   padding: 30px;
-`
-
-const Bar = styled.div `
-  margin-bottom: 30px;
-  display: grid;
-  grid-template-columns: 180px auto 100px 100px;
 `
 
 const Content = styled.div `
@@ -73,19 +58,7 @@ settingsContent = () => {
   render() {
     return (
       <AppLayout>
-        <Bar>
-          <Logo>
-            CryptoDash
-          </Logo>
-          <div>
-          </div>
-          {!this.state.firstVisit && (<ControlButton onClick={()=>{this.setState({page: 'dashboard'})}}active={this.displayingDashboard()}>
-            Dashboard
-          </ControlButton>)}
-          <ControlButton onClick={()=>{this.setState({page: 'settings'})}}active={this.displayingSettings()}>
-          Settings
-          </ControlButton>
-        </Bar>
+        {AppBar.call(this)}
         <Content>
           {this.displayingSettings() && this.settingsContent()}
         </Content>
